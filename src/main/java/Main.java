0fab2bc33c7f99a5fc1b7b80e6469dbf9148861c
tanjakatariina <tanja.k.pyykonen@gmail.com>
2022-10-18 14:54:34 +0300

    Added creating and closing SessionFactory.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
-
-
+import util.HibernateUtil;
 import view.MainGui;
 
 public class Main {
-
 	public static void main(String[] args) {
+		HibernateUtil.createSessionFactory();
 		MainGui.main(args);
+		HibernateUtil.closeSessionFactory();
 	}
-
 }
